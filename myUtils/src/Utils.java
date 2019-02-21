a7aed2a1d8c9820baa7dca59deb314afbdcc7964
guxw <guxw@asiainfo.com>
2019-02-21 16:32:40 +0800

    捕获异常并打印

--- myUtils/src/Utils.java
+++ myUtils/src/Utils.java
@@ -1,3 +1,5 @@
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -12,5 +14,30 @@ public class Utils {
             values.add(map.get(it.next().toString()));
         }
         return values;
+    }
+
+    /**
+     *
+     * 功能描述: 答应完整错误信息
+     *
+     * @param:
+     * @return:
+     * @author: guxw
+     * @date: 2019/2/21 16:30
+     */
+    public static void main(String[] args) {
+        try {
+            String aa = "";
+            System.out.println(aa.substring(3));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw, true));
+            String str = sw.toString();
+            System.out.println("==========");
+
+            System.out.println(str);
+        }
     }
 }
