47b01c2af880c0ac6483299ef562418bc6687ca2
guxw <onthewaygu@163.com>
2019-06-20 17:07:51 +0800

    --cacahe实验

---
+++ haha.sh
@@ -0,0 +1,24 @@
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+	modified:   myUtils/.idea/workspace.xml
+	modified:   myUtils/src/Java2xml.java
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+	haha.sh
+	myUtils/.idea/inspectionProfiles/
+	myUtils/.idea/uiDesigner.xml
+	myUtils/hs_err_pid14392.log
+	myUtils/out/production/myUtils/Excel2Message.class
+	myUtils/out/production/myUtils/Java2xml.class
+	myUtils/src/Excel2Message.java
+	myUtils/src/SSLSupports.java
+	remote/
+
+no changes added to commit (use "git add" and/or "git commit -a")
